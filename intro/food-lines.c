bad3dda3c25d4f5c48b943b36d9dd06e0023b930
Jon Johnson <jonjohnsontc@live.com>
2024-05-03 16:16:13 -0700

    thinking I have a working version of the intro problem

--- intro/food-lines.c
+++ intro/food-lines.c
@@ -10,14 +10,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAXSIZE 512
+
 int main() {
   int ppl, num_lines;
+  char input[MAXSIZE];
+  fgets(input, MAXSIZE, stdin);
+  num_lines = atoi(strtok(input, " "));
+  ppl = atoi(strtok(NULL, " "));
 
-  int *lines = malloc(num_lines * sizeof(int));
+  printf("num_lines read in is %d\n", num_lines);
+  printf("ppl is %d\n", ppl);
+  int *lines = malloc(num_lines);
+  
+  // initialize lines to 0
   for (int i = 0; i < num_lines; i++)
     lines[i] = 0;
-  for (int j = 0; j < ppl; j++)
-    lines[j] += 1;
+
+  // populate lines with people
+  for (int ln = 0, person = 0; person < ppl; person++, ln++) {
+    if (ln == num_lines)
+      ln = 0;
+    lines[ln] += 1;
+  }
+
   for (int ln = 0; ln < num_lines; ln++)
-    printf("%d ", ln);
+    printf("%d ", lines[ln]);
+  printf("\n");
 }
