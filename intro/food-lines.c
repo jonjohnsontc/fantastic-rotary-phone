aa16ce8a39792092f1f136ed3232bf6324e7e0bf
Jon Johnson <jonjohnsontc@live.com>
2024-05-03 17:11:14 -0700

    trying to figure out why i'm segfaulting

--- intro/food-lines.c
+++ intro/food-lines.c
@@ -7,6 +7,8 @@
 
 // N is the first int
 // M is the second int
+// we get a second line with N number of positive
+// integers with current lengths of lines
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,16 +19,29 @@ int main() {
   int ppl, num_lines;
   char input[MAXSIZE];
   fgets(input, MAXSIZE, stdin);
+  printf("After first fgets call, input is %s\n", input);
   num_lines = atoi(strtok(input, " "));
   ppl = atoi(strtok(NULL, " "));
 
   printf("num_lines read in is %d\n", num_lines);
   printf("ppl is %d\n", ppl);
   int *lines = malloc(num_lines);
-  
-  // initialize lines to 0
-  for (int i = 0; i < num_lines; i++)
-    lines[i] = 0;
+
+  fgets(input, MAXSIZE, stdin);
+  printf("After second fgets call, input is %s\n", input);
+  for (int i = 0; i <= num_lines; i++) {
+    if (i % 2 == 0) 
+      lines[i] = atoi(strtok(input, " "));
+    else
+      lines[i] = atoi(strtok(NULL, " "));
+    printf("index %d is %d\n", i, lines[i]);
+  }
+
+  printf("Initial lines is\n");
+  for (int j = 0; j < num_lines; j++) {
+    printf("%d ", lines[j]);
+  }
+  printf("\n");
 
   // populate lines with people
   for (int ln = 0, person = 0; person < ppl; person++, ln++) {
