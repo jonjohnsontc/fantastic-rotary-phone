fa45e2b5d34327b6375b7c79e85350ce046ebebe
Jon Johnson <jonjohnsontc@live.com>
2024-05-03 17:41:38 -0700

    i'm making some silly mistake

--- intro/food-lines.c
+++ intro/food-lines.c
@@ -15,42 +15,45 @@
 #include <string.h>
 #define MAXSIZE 512
 
+int shortest_ln(int *arr, int len);
+int min(int a, int b);
+
 int main() {
   int ppl, num_lines;
   char input[MAXSIZE];
   fgets(input, MAXSIZE, stdin);
-  printf("After first fgets call, input is %s\n", input);
   num_lines = atoi(strtok(input, " "));
   ppl = atoi(strtok(NULL, " "));
-
-  printf("num_lines read in is %d\n", num_lines);
-  printf("ppl is %d\n", ppl);
+  
   int *lines = malloc(num_lines);
 
   fgets(input, MAXSIZE, stdin);
-  printf("After second fgets call, input is %s\n", input);
-  for (int i = 0; i <= num_lines; i++) {
-    if (i % 2 == 0) 
-      lines[i] = atoi(strtok(input, " "));
-    else
-      lines[i] = atoi(strtok(NULL, " "));
-    printf("index %d is %d\n", i, lines[i]);
+  lines[0] = atoi(strtok(input, " "));
+  for (int i = 1; i < num_lines; i++) {
+    lines[i] = atoi(strtok(NULL, " "));
   }
-
-  printf("Initial lines is\n");
+  printf("Initial lines:\n");
   for (int j = 0; j < num_lines; j++) {
     printf("%d ", lines[j]);
   }
   printf("\n");
 
-  // populate lines with people
-  for (int ln = 0, person = 0; person < ppl; person++, ln++) {
-    if (ln == num_lines)
-      ln = 0;
-    lines[ln] += 1;
+  for (; ppl > 0; ppl--) {
+    int idx = shortest_ln(lines, num_lines);
+    printf("%d\n", lines[idx]);
+    lines[idx]++;
   }
-
-  for (int ln = 0; ln < num_lines; ln++)
-    printf("%d ", lines[ln]);
-  printf("\n");
 }
+
+int shortest_ln(int *arr, int len) {
+  int shorty = 101;
+  int i;
+  for (i = 0; i < len; i++) {
+    printf("Comparing %d and %d\n", shorty, arr[i]);
+    shorty = min(shorty, arr[i]);
+    printf("Min is %d\n", shorty);
+  }
+  return i;
+}
+
+int min(int a, int b) {return a < b ? a : b;}
