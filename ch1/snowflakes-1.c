0315e7874f5227e696a615a3ada0ff179231dc86
Jon Johnson <jonjohnsontc@live.com>
2024-05-04 07:26:44 -0700

    additions

--- ch1/snowflakes-1.c
+++ ch1/snowflakes-1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #define MAX_FLAKES 100000 /* max number of snowflakes to be read from input */
 
+int identical_right(int cmp1[], int cmp2[], int start_i);
+int identical_left(int cmp1[], int cmp2[], int start_i);
+
 void identify_identical(int nums[], int len) {
   int i, j;
   for (i = 0; i < len; i++) {
@@ -12,6 +15,18 @@ void identify_identical(int nums[], int len) {
     }
   }
   printf("No two snowflakes are alike.\n");
+}
+
+int identical_right(int cmp1[], int cmp2[], int start_i) {
+  int offset;
+  for (offset = 0; offset < 6; offset++) {
+    // leveraging the modulo allows us to mimic array wraparound
+    // behavior for indices >= 6
+    if (cmp1[offset] != cmp2[start_i + offset % 6]) {
+      return 0; /* return false */
+    }
+  }
+  return 1; /* return true */
 }
 
 int main(void) {
