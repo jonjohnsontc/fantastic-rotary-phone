beff7056028d559779f2989408d31c97270cdabe
Jon Johnson <jonjohnsontc@live.com>
2024-05-24 13:28:14 -0700

    finished memoization portion of 2nd problem in chapter 3

--- ch3/low-price.c
+++ ch3/low-price.c
@@ -14,34 +14,38 @@ double min(double v1, double v2) {
     return v2;
 }
 
-// return min cost for buying exactly num_apples
 double solve_k(int num[], double price[], int num_schemes, double unit_price,
-               int num_apples) {
+               int num_apples, double memo[]) {
   double best, result;
   int i;
-  if (num_apples == 0)
+  if (memo[num_apples] != -1)
+    return memo[num_apples];
+  if (num_apples == 0) {
+    memo[num_apples] = 0;
     return 0;
-  else {
-    result = solve_k(num, price, num_schemes, unit_price, num_apples - 1);
+  } else {
+    result = solve_k(num, price, num_schemes, unit_price, num_apples - 1, memo);
     best = result + unit_price;
     for (i = 0; i < num_schemes; i++)
       if (num_apples - num[i] >= 0) {
-        result = solve_k(num, price, num_schemes, unit_price, num_apples);
+        result =
+            solve_k(num, price, num_schemes, unit_price, num_apples - num[i], memo);
         best = min(best, result + price[i]);
       }
-    return best;
+    memo[num_apples] = best;
+    return memo[num_apples];
   }
 }
 
 #define SIZE 200
 
 double solve(int num[], double price[], int num_schemes, double unit_price,
-             int num_apples) {
+             int num_apples, double memo[]) {
   double best;
   int i;
-  best = solve_k(num, price, num_schemes, unit_price, num_apples);
+  best = solve_k(num, price, num_schemes, unit_price, num_apples, memo);
   for (i = num_apples + 1; i < SIZE; i++)
-    best = min(best, solve_k(num, price, num_schemes, unit_price, i));
+    best = min(best, solve_k(num, price, num_schemes, unit_price, i, memo));
   return best;
 }
 
@@ -65,19 +69,22 @@ int main(void) {
   double unit_price, result;
   int num[MAX_SCHEMES];
   double price[MAX_SCHEMES];
+  double memo[SIZE];
   test_case = 0;
   while (scanf("%lf%d ", &unit_price, &num_schemes) != -1) {
     test_case++;
     for (i = 0; i < num_schemes; i++)
       scanf("%d%lf ", &num[i], &price[i]);
     printf("Case %d:\n", test_case);
+    for (i = 0; i < SIZE; i++) 
+      memo[i] = -1;
     more = get_number(&num_apples);
     while (more) {
-      result = solve(num, price, num_schemes, unit_price, num_apples);
+      result = solve(num, price, num_schemes, unit_price, num_apples, memo);
       printf("Buy %d for $%.2f\n", num_apples, result);
       more = get_number(&num_apples);
     }
-    result = solve(num, price, num_schemes, unit_price, num_apples);
+    result = solve(num, price, num_schemes, unit_price, num_apples, memo);
     printf("Buy %d for $%.2f\n", num_apples, result);
   }
   return 0;
