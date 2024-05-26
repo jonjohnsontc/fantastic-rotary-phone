a07b9e0add31539c4d7e0f4bd0717f824a1379c2
Jon Johnson <jonjohnsontc@live.com>
2024-05-26 13:30:24 -0700

    optimized memory for solution 3

--- ch3/3/solution_3.c
+++ ch3/3/solution_3.c
@@ -10,26 +10,27 @@ int max(int v1, int v2) {
 #define SIZE 1000
 
 int solve(char outcome1[], char outcome2[], int goals1[], int goals2[], int n) {
-  int i, j;
+  int i, j, k;
   int first, second, third, fourth;
-  static int dp[SIZE + 1][SIZE + 1];
+  static int previous[SIZE + 1], current[SIZE + 1];
   for (i = 0; i <= n; i++)
-    dp[0][i] = 0;
-  for (i = 0; i <= n; i++)
-    dp[i][0] = 0;
-  for (i = 1; i <= n; i++)
+    previous[i] = 0;
+  for (i = 1; i <= n; i++) {
     for (j = 1; j <= n; j++) {
       if ((outcome1[i] == 'W' && outcome2[j] == 'L' && goals1[i] > goals2[j]) ||
           (outcome1[i] == 'L' && outcome2[j] == 'W' && goals1[i] < goals2[j]))
-        first = dp[i - 1][j - 1] + goals1[i] + goals2[j];
+        first = previous[j - 1] + goals1[i] + goals2[j];
       else
         first = 0;
-      second = dp[i - 1][j - 1];
-      third = dp[i - 1][j];
-      fourth = dp[i][j - 1];
-      dp[i][j] = max(first, max(second, max(third, fourth)));
+      second = previous[j - 1];
+      third = previous[j];
+      fourth = current[j - 1];
+      current[j] = max(first, max(second, max(third, fourth)));
     }
-  return dp[n][n];
+    for (k = 0; k <= SIZE; k++)
+      previous[k] = current[k];
+  }
+  return current[n];
 }
 
 int main(void) {
