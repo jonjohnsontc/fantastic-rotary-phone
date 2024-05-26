4370294bdd688827c6a6ef17daadb7ff1ab8064b
Jon Johnson <jonjohnsontc@live.com>
2024-05-26 11:54:04 -0700

    recursive solution working for problem 3 chapter 3

--- ch3/3/solution_1.c
+++ ch3/3/solution_1.c
@@ -2,7 +2,7 @@
 // arrays store games starting in index 1, rather than index 0
 // to match up with games played.
 
-
+#include <stdio.h>
 int max(int v1, int v2) {
   if (v1 > v2)
     return v1;
@@ -25,4 +25,24 @@ int solve(char outcome1[], char outcome2[], int goals1[], int goals2[], int i,
   third = solve(outcome1, outcome2, goals1, goals2, i - 1, j);
   fourth = solve(outcome1, outcome2, goals1, goals2, i, j - 1);
   return max(first, max(second, max(third, fourth)));
+}
+
+#define SIZE 1000
+
+int main(void) {
+  int i, n, result;
+  char outcome1[SIZE + 1], outcome2[SIZE + 1];
+  int goals1[SIZE + 1], goals2[SIZE + 1];
+  scanf("%d ", &n);
+  for (i = 1; i <= n; i++)
+    scanf("%c", &outcome1[i]);
+  for (i = 1; i <= n; i++)
+    scanf("%d ", &goals1[i]);
+  for (i = 1; i <= n; i++)
+    scanf("%c", &outcome2[i]);
+  for (i = 1; i <= n; i++)
+    scanf("%d", &goals2[i]);
+  result = solve(outcome1, outcome2, goals1, goals2, n, n);
+  printf("%d\n", result);
+  return 0;
 }
