275c04b6bd4ab0bfa80b2fabc9c776418317e4c3
Jon Johnson <jonjohnsontc@live.com>
2024-05-29 17:43:37 -0700

    work on nikola dp solution

--- ch4/1/nikola_solution_3.c
+++ ch4/1/nikola_solution_3.c
@@ -1,17 +1,49 @@
 // DP solution to nikola problem
 #include <stdio.h>
 #define SIZE 1000
+#define UNSET -2
+#define IMPOSSIBLE -1
+int min(int v1, int v2) {
+  if (v1 < v2)
+    return v1;
+  else
+    return v2;
+}
+
+int solve(int cost[], int n, int i, int j, int dp[SIZE + 1][SIZE + 1]) {
+  int first, second;
+  first = 0;
+  second = 0;
+  if (first == -1 && second == -1) {
+    dp[i][j] = -1;
+    return dp[i][j];
+  } else if (second == -1) {
+    dp[i][j] = first + cost[i];
+    return dp[i][j];
+  } else if (first == -1) {
+    dp[i][j] = second + cost[i];
+  } else {
+    dp[i][j] = min(first, second) + cost[i];
+  }
+
+  return -1;
+}
 
 int main(void) {
-  // static int dp[SIZE + 1][SIZE + 1];
+  static int dp[SIZE + 1][SIZE + 1];
   int cost[SIZE + 1];
-  int n, i, j;
+  int n, i, j, result;
   scanf("%d ", &n);
   for (i = 0; i < n; i++)
     scanf("%d", &cost[i]);
-  
+
+  for (i = 1; i < SIZE + 1; i++)
+    for (j = 1; j < SIZE + 1; j++)
+      dp[i][j] = UNSET;
+
   for (j = n; j >= 1; j--)
     for (i = 1; i < n; i++) {
-      printf("j = %d, i = %d\n", j, i);
-  }
+      result = solve(cost, n, i, j, dp);
+    }
+  printf("%d\n", result);
 }
