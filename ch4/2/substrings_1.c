a6d46e0f1c19a21c30f11f2bb9917757a353f5ee
Jon Johnson <jonjohnsontc@live.com>
2024-06-06 18:07:06 -0700

    finishing up solution 1 for exercise 2 in ch4

--- ch4/2/substrings_1.c
+++ ch4/2/substrings_1.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #define MAX_A 1000
 #define MAX_B 200
 #define MAX_K 200
@@ -9,21 +10,21 @@
 int solve_ijk(char a[], char b[], int i, int j, int k,
               int memo[MAX_A][MAX_B][MAX_K + 1]) {
   int total, q;
-//   memoized check
+  //   memoized check
   if (memo[i][j][k] != -1)
     return memo[i][j][k];
-// if we only need one substring and it's length is 1
-// and the indices are equal, we've got a singular match
+  // if we only need one substring and it's length is 1
+  // and the indices are equal, we've got a singular match
   if (j == 0 && k == 1 && a[i] == b[j]) {
     memo[i][j][k] = 1;
     return memo[i][j][k];
   }
-//  all impossible states to get a substring that matches
+  //  all impossible states to get a substring that matches
   if (i == 0 || j == 0 || k == 0) {
     memo[i][j][k] = 0;
     return memo[i][j][k];
   }
-// if final chars don't match, we can't solve in this manner  
+  // if final chars don't match, we can't solve in this manner
   if (a[i] != b[j]) {
     memo[i][j][k] = 0;
     return memo[i][j][k];
@@ -34,4 +35,29 @@ int solve_ijk(char a[], char b[], int i, int j, int k,
   total = (total + solve_ijk(a, b, i - 1, j - 1, k, memo)) % MOD;
   memo[i][j][k] = total;
   return memo[i][j][k];
+}
+
+int solve(char a[], char b[], int a_length, int b_length, int num_substrings) {
+  int i, j, k, result;
+  static int memo[MAX_A][MAX_B][MAX_K + 1];
+  for (i = 0; i < a_length; i++)
+    for (j = 0; j < b_length; j++)
+      for (k = 0; k <= num_substrings; k++)
+        memo[i][j][k] = -1;
+  result = 0;
+  for (i = 0; i < a_length; i++) {
+    result = result + solve_ijk(a, b, i, b_length - 1, num_substrings, memo);
+    result = result % MOD;
+  }
+  return result;
+}
+
+int main(void) {
+  int a_length, b_length, num_substrings;
+  char a[MAX_A + 1], b[MAX_B + 1];
+  scanf("%d%d%d", &a_length, &b_length, &num_substrings);
+  scanf("%s", a);
+  scanf("%s", b);
+  printf("%d\n", solve(a, b, a_length, b_length, num_substrings));
+  return 0;
 }
