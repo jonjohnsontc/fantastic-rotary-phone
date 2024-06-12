de71fb63494a72af6e6ab21ecfed8dcf4ca5ebae
Jon Johnson <jonjohnsontc@live.com>
2024-06-12 16:54:22 -0700

    adding solution 2 and the beginning of 1 in ch5

---
+++ ch5/2/reach_1.c
@@ -0,0 +1,13 @@
+/*
+    Reach for the Top
+    https://dmoj.ca/problem/wc18c1s3
+
+    Bob needs to climb a rope in gym class
+
+    - required height >= H meters
+    - he can go up (jump) J meters
+    - he can fall down any # of meters
+    - he can't touch any portions w/ itching powder
+
+`   min number of moves to get to height h or heigher
+*/
