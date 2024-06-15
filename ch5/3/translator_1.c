1fb2683fe916a0bae99fa943192fb9c19f48661c
Jon Johnson <jonjohnsontc@live.com>
2024-06-14 19:09:37 -0700

    more work on problem 3

--- ch5/3/translator_1.c
+++ ch5/3/translator_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/_types/_null.h>
 
 char *read_word(int size) {
   char *str;
@@ -23,4 +24,59 @@ char *read_word(int size) {
   }
   str[len] = '\0';
   return str;
+}
+
+#define MAX_LANGS 101
+#define WORD_LENGTH 16
+
+typedef struct edge {
+  int to_lang, cost;
+  struct edge *next;
+} edge;
+
+typedef int board[MAX_LANGS];
+typedef int positions[MAX_LANGS];
+
+int main(void) {
+  static edge *adj_list[MAX_LANGS] = {NULL};
+  static char *lang_names[MAX_LANGS];
+  int i, num_targets, num_translators, cost, from_index, to_index;
+  char *from_lang, *to_lang;
+  edge *e;
+  static board min_costs;
+  scanf("%d%d", &num_targets, &num_translators);
+  lang_names[0] = "English"; // we start with english
+
+  for (i = 1; i <= num_targets; i++) {
+    lang_names[i] = read_word(WORD_LENGTH);
+  }
+
+  for (i = 0; i < num_translators; i++) {
+    from_lang = read_word(WORD_LENGTH);
+    to_lang = read_word(WORD_LENGTH);
+    scanf("%d ", &cost);
+    from_index = find_lang(lang_names, from_lang);
+    to_index = find_lang(lang_names, to_lang);
+    e = malloc(sizeof(edge));
+    if (e == NULL) {
+      fprintf(stderr, "malloc error\n");
+      exit(1);
+    }
+    e->to_lang = to_index;
+    e->cost = cost;
+    e->next = adj_list[from_index];
+    adj_list[from_index] = e;
+    e = malloc(sizeof(edge));
+    if (e == NULL) {
+      fprintf(stderr, "malloc error\n");
+      exit(1);
+    }
+    e->to_lang = from_index;
+    e->cost = cost;
+    e->next = adj_list[to_index];
+    adj_list[to_index] = e;
+  }
+  find_distances(adj_list, num_targets + 1, min_costs);
+  solve(num_targets + 1, min_costs);
+  return 0;
 }
