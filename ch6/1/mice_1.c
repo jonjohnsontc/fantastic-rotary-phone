ec669ed951d377716f08fc03680e132dc3e7a6af
Jon Johnson <jonjohnsontc@live.com>
2024-06-17 20:09:10 -0700

    adding djikstras algo

--- ch6/1/mice_1.c
+++ ch6/1/mice_1.c
@@ -7,9 +7,42 @@ typedef struct edge {
   struct edge *next;
 } edge;
 
-int find_time(edge *adj_list[MAX_CELLS + 1], int num_cells, int cell_num,
-              int exit_cell) {
-  return -1;
+int find_time(edge *adj_list[], int num_cells, int from_cell, int exit_cell) {
+  static int done[MAX_CELLS + 1];
+  static int min_times[MAX_CELLS + 1];
+  int i, j, found;
+  int min_time, min_time_index, old_time;
+  edge *e;
+  for (i = 1; i <= num_cells; i++) {
+    done[i] = 0;
+    min_times[i] = -1;
+  }
+  min_times[from_cell] = 0;
+  for (i = 0; i < num_cells; i++) {
+    min_time = -1;
+    found = 0;
+    for (j = 1; j <= num_cells; j++) {
+      if (!done[j] && min_times[j] >= 0) {
+        if (min_time == -1 || min_times[j] < min_time) {
+          min_time = min_times[j];
+          min_time_index = j;
+          found = 1;
+        }
+      }
+    }
+    if (!found)
+      break;
+    done[min_time_index] = 1;
+
+    e = adj_list[min_time_index];
+    while (e) {
+      old_time = min_times[e->to_cell];
+      if (old_time == -1 || old_time > min_time + e->length)
+        min_times[e->to_cell] = min_time + e->length;
+      e = e->next;
+    }
+  }
+  return min_times[exit_cell];
 }
 
 int main(void) {
