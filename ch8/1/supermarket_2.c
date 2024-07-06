a6ee8599cc0e849c6b37f7ffc0a4f27e39690fc9
Jon Johnson <jonjohnsontc@live.com>
2024-07-06 07:54:23 -0700

    editing max heap based approach

--- ch8/1/supermarket_2.c
+++ ch8/1/supermarket_2.c
@@ -1,59 +1,47 @@
 /*
-  max-heap based approach to solving supermarket
+  Max-Heap (as array) solve for supermarket promotion
 */
 #include <stdio.h>
 #define MAX_RECEIPTS 1000000
 #define MAX_COST 1000000
 
-typedef struct receipt {
+typedef struct heap_element {
+  int receipt_index;
   int cost;
-  int used;
-} receipt;
+} heap_element;
 
-int extract_max(receipt receipts[], int num_receipts) {
-  int max, max_index, i;
-  max = -1;
-  for (i = 0; i < num_receipts; i++) {
-    if (!receipts[i].used && receipts[i].cost > max) {
-      max_index = i;
-      max = receipts[i].cost;
-    }
+void max_heap_insert(heap_element heap[], int *num_heap, int receipt_index,
+                     int cost) {
+  int i;
+  heap_element temp;
+  (*num_heap)++;
+  heap[*num_heap] = (heap_element){receipt_index, cost};
+  i = *num_heap;
+  while (i > 1 && heap[i].cost > heap[i / 2].cost) {
+    temp = heap[i];
+    heap[i] = heap[i / 2];
+    heap[i / 2] = temp;
+    i = i / 2;
   }
-  receipts[max_index].used = 1;
-  return max;
 }
 
-int extract_min(receipt receipts[], int num_receipts) {
-  int min, min_index, i;
-  min = MAX_COST + 1;
-  for (i = 0; i < num_receipts; i++)
-    if (!receipts[i].used && receipts[i].cost < min) {
-      min_index = i;
-      min = receipts[i].cost;
+heap_element max_heap_extract(heap_element heap[], int *num_heap) {
+  heap_element remove, temp;
+  int i, child;
+  remove = heap[1];
+  heap[1] = heap[*num_heap];
+  (*num_heap)--;
+  i = 1;
+  while (i * 2 <= *num_heap) {
+    child = i * 2;
+    if (child < *num_heap && heap[child + 1].cost > heap[child].cost) {
+      temp = heap[i];
+      heap[i] = heap[child];
+      heap[child] = temp;
+      i = child;
+    } else {
+      break;
     }
-  receipts[min_index].used = 1;
-  return min;
-}
-
-int main(void) {
-  static struct receipt receipts[MAX_RECEIPTS];
-  int num_days, num_receipts_today;
-  int num_receipts = 0;
-  long long total_prizes = 0;
-  int i, j, max, min;
-  scanf("%d", &num_days);
-
-  for (i = 0; i < num_days; i++) {
-    scanf("%d", &num_receipts_today);
-    for (j = 0; j < num_receipts_today; j++) {
-      scanf("%d", &receipts[num_receipts].cost);
-      receipts[num_receipts].used = 0;
-      num_receipts++;
-    }
-    max = extract_max(receipts, num_receipts);
-    min = extract_min(receipts, num_receipts);
-    total_prizes += max - min;
   }
-  printf("%lld\n", total_prizes);
-  return 0;
+  return remove;
 }
