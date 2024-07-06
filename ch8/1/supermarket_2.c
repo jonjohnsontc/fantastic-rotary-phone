2e42c7b7b4c29a1b4b366dd9c2af746fe8a4e4d2
Jon Johnson <jonjohnsontc@live.com>
2024-07-06 09:28:41 -0700

    working on 8-1

--- ch8/1/supermarket_2.c
+++ ch8/1/supermarket_2.c
@@ -45,3 +45,44 @@ heap_element max_heap_extract(heap_element heap[], int *num_heap) {
   }
   return remove;
 }
+
+// min_heap functions have been defined separately
+// normally, we'd have a generic binary heap, and pass through some sort of
+// comparison function, but will be separated in this example
+void min_heap_insert(heap_element heap[], int *num_heap, int receipt_index,
+                     int cost) {
+  int i;
+  heap_element temp;
+  (*num_heap)++;
+  heap[*num_heap] = (heap_element){receipt_index, cost};
+  i = *num_heap;
+  while (i > 1 && heap[i].cost < heap[i / 2].cost) {
+    temp = heap[i];
+    heap[i] = heap[i / 2];
+    heap[i / 2] = temp;
+    i = i / 2;
+  }
+}
+
+heap_element min_heap_extract(heap_element heap[], int *num_heap) {
+  heap_element remove, temp;
+  int i, child;
+  remove = heap[1];
+  heap[1] = heap[*num_heap];
+  (*num_heap)--;
+  i = 1;
+  while (i * 2 <= *num_heap) {
+    child = i * 2;
+    if (child < *num_heap && heap[child + 1].cost < heap[child].cost)
+      child++;
+    if (heap[child].cost < heap[i].cost) {
+      temp = heap[i];
+      heap[i] = heap[child];
+      heap[child] = temp;
+      i = child;
+    } else {
+      break;
+    }
+  }
+  return remove;
+}
