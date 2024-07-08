4031fb725897e49c0c0bab334bc1bdf1c13f4b46
Jon Johnson <jonjohnsontc@live.com>
2024-07-08 15:04:47 -0700

    adding more code for 8-2 attempt 1

--- ch8/2/treap_1.c
+++ ch8/2/treap_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_NODES 50000
 #define LABEL_LENGTH 16
 typedef struct treap_node {
@@ -29,6 +30,35 @@ char *read_label(int size) {
   }
   str[len] = '\0';
   return str;
+}
+
+int compare(const void *v1, const void *v2) {
+  const treap_node *n1 = v1;
+  const treap_node *n2 = v2;
+  return strcmp(n1->label, n2->label);
+}
+
+int max_priority_index(treap_node treap_nodes[], int left, int right) {
+  int i;
+  int max_index = left;
+  for (i = left + 1; i <= right; i++)
+    if (treap_nodes[i].priority > treap_nodes[max_index].priority)
+      max_index = i;
+  return max_index;
+}
+
+void solve(treap_node treap_nodes[], int left, int right) {
+  int root_index;
+  treap_node root;
+  if (left > right)
+    return;
+  root_index = max_priority_index(treap_nodes, left, right);
+  root = treap_nodes[root_index];
+  printf("(");
+  solve(treap_nodes, left, root_index - 1);
+  printf("%s/%d", root.label, root.priority);
+  solve(treap_nodes, root_index + 1, right);
+  printf(")");
 }
 
 int main(void) {
