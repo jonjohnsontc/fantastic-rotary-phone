49b1aa479d3e39dc55425f6245dcc7ab48dcbd74
Jon Johnson <jonjohnsontc@live.com>
2024-07-14 17:29:43 -0700

    adding first attempt at a solution to the social network problem (9-1)

---
+++ ch9/1/social_0.c
@@ -0,0 +1,39 @@
+/*
+
+Implementing a Social Network supporting three different types of queries:
+
+- A x y - x sends a friend request to y
+- E x y - check whether x and y are part of the same community
+- S x - prints the size of the community x belongs to
+
+Friend requests are excepted if the merger of the two communities is less than
+the maximum size of the community 'm'.
+
+First line contains n and m
+next line is q (no of queries)
+q lines of queries follows
+
+In my head, I would want to only have one copy of the entire social network
+available, and then associate some value with what communities it was a part of
+
+Initially, every member of the social network will be a part of their own
+community of 1. It will then grow according to the number of friend requests it
+and associated members gets
+
+Queries are 1-indexed
+
+The book intros this as a problem for the union-find data structure,
+an expansion on the adjacency list data structure used to help solve graph
+problems in chapters 5 and 6.
+
+An adjacency list maps edges between nodes out in an array, where each node
+is an index in the edge array, and the edges have an integer representing the
+node they travel to, as well as a length and reference to the next edge.
+*/
+#define M_MAX 100000
+#define QUERY_MAX 200000
+typedef struct community {
+  int size;
+  struct community *next;
+} community;
+int main(void) { community network[M_MAX + 1]; }
