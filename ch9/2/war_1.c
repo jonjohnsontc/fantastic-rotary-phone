3e3ebf779db59df3aefaa99ebb274fce368d039e
Jon Johnson <jonjohnsontc@live.com>
2024-07-27 10:21:36 -0700

    checkpoint

--- ch9/2/war_1.c
+++ ch9/2/war_1.c
@@ -22,22 +22,22 @@ int main(void) {
   scanf("%d%d%d", &op, &person1, &person2);
 
   while (op != 0) {
-    if (op == 1) // set friends
+    if (op == 1) {
       if (are_enemies(person1, person2, parent, enemy_of))
         printf("-1\n");
       else
         set_friends(person1, person2, parent, size, enemy_of);
-    else if (op == 2) // set enemies
+    } else if (op == 2) { // set enemies
       if (are_friends(person1, person2, parent))
         printf("-1\n");
       else
         set_enemies(person1, person2, parent, size, enemy_of);
-    else if (op == 3) // are friends
+    } else if (op == 3) { // are friends
       if (are_friends(person1, person2, parent))
         printf("-1\n");
       else
         printf("0\n");
-    else if (op == 4) { // are enemies
+    } else if (op == 4) { // are enemies
       if (are_enemies(person1, person2, parent, enemy_of))
         printf("1\n");
       else
@@ -46,4 +46,83 @@ int main(void) {
     scanf("%d%d%d", &op, &person1, &person2);
   }
   return 0;
+}
+
+int find(int person, int parent[]) {
+  int set = person, temp;
+  while (parent[set] != set)
+    set = parent[set];
+  // set the root for all in-between nodes, so that
+  // we have O(1) performance to find their parent
+  while (parent[person] != set) {
+    temp = parent[person];
+    parent[person] = set;
+    person = temp;
+  }
+  return set;
+}
+
+int union_sets(int person1, int person2, int parent[], int size[]) {
+  int set1, set2, temp;
+  set1 = find(person1, parent);
+  set2 = find(person1, parent);
+  if (set1 != set2) {
+    // union sets
+    // set2 will always be the parent, so if set1 is larger
+    // we'll switch the values of set1 and set2
+    if (size[set1] > size[set2]) {
+      temp = set1;
+      set1 = set2;
+      set2 = temp;
+    }
+    parent[set1] = set2;
+    size[set2] = size[set2] + size[set1];
+  }
+  return set2;
+}
+
+void set_friends(int person1, int person2, int *parent, int *size,
+                 int *enemy_of) {
+  int set1, set2, bigger_set, other_set;
+  set1 = find(person1, parent);
+  set2 = find(person2, parent);
+  bigger_set = union_sets(person1, person2, parent, size);
+  if (enemy_of[set1] != -1 && enemy_of[set2] != -1)
+    union_sets(enemy_of[set1], enemy_of[set2], parent, size);
+  if (bigger_set == set1)
+    other_set = set2;
+  else
+    other_set = set1;
+  if (enemy_of[bigger_set] == -1)
+    enemy_of[bigger_set] = enemy_of[other_set];
+}
+
+void set_enemies(int person1, int person2, int *parent, int *size,
+                 int *enemy_of) {
+  int set1, set2, enemy;
+  set1 = find(person1, parent);
+  set2 = find(person2, parent);
+
+  enemy = enemy_of[set1];
+  if (enemy == -1)
+    enemy_of[set1] = person2;
+  else
+    union_sets(enemy, person2, parent, size);
+
+  enemy = enemy_of[set2];
+  if (enemy == -1)
+    enemy_of[set2] = person1;
+  else
+    union_sets(enemy, person1, parent, size);
+}
+
+int are_friends(int person1, int person2, int *parent) {
+  return find(person1, parent) == find(person2, parent);
+}
+
+int are_enemies(int person1, int person2, int *parent, int *enemy_of) {
+  int set1, enemy;
+  set1 = find(person1, parent);
+  enemy = enemy_of[set1];
+  return (enemy != -1) && (find(enemy, parent) == find(person2, parent));
 }
