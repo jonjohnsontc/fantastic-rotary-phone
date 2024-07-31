49209de5892b787d680d7c76a0715f1e3d2443d7
Jon Johnson <jonjohnsontc@live.com>
2024-07-30 17:51:04 -0700

    self attempt of 9-3

--- ch9/3/drawers.c
+++ ch9/3/drawers.c
@@ -10,7 +10,7 @@ typedef struct {
   bool is_set;
 } Item;
 
-void insert(Item *drawers, int left, int right);
+void insert(Item *drawers, int id, int left, int right);
 int main(void) {
   static Item drawers[MAX_DRAWERS + 1];
   int no_items, no_drawers;
@@ -19,22 +19,25 @@ int main(void) {
   scanf("%d%d", &no_items, &no_drawers);
   for (item = 1; item < no_items; item++) {
     scanf("%d%d", &left, &right);
-    insert(drawers, left, right);
+    insert(drawers, item, left, right);
   }
   return 0;
 }
 
-void insert(Item *drawers, int item, int left, int right) {
-  if (!drawers[left].is_set) {
-    drawers[left].id = item;
-    drawers[left].other_drawer = right;
-    drawers[left].is_set = true;
-
-  } else if (!drawers[right].is_set) {
-  }
+void set_drawer(Item *drawers, int id, int location, int other_drawer) {
+  drawers[location].id = id;
+  drawers[location].other_drawer = other_drawer;
+  drawers[location].is_set = true;
+  printf("LADICA");
 }
 
-void set_drawer(Item *drawers, int id, int location, int other_drawer) {
-
-  printf("LADICA\n");
+void insert(Item *drawers, int item, int left, int right) {
+  if (!drawers[left].is_set) {
+    set_drawer(drawers, item, left, right);
+  } else if (!drawers[right].is_set) {
+    set_drawer(drawers, item, right, left);
+  } else {
+    insert(drawers, drawers[left].id, left, drawers[left].other_drawer);
+    insert(drawers, drawers[right].id, right, drawers[right].other_drawer);
+  }
 }
